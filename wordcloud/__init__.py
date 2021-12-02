4ab3cbd0bb3420da718e048c5c4c3a2f09ee83be
Va1ue-0268 <mirai0000@qq.com>
2021-12-02 11:15:37 +0800

    加一个分词，词云and句云

--- wordcloud/__init__.py
+++ wordcloud/__init__.py
@@ -1,22 +1,26 @@
-import os
-import nonebot
 import json
+import base64
+import string
+import numpy as np
+from PIL import Image
+from io import BytesIO
+
 from nonebot import on_command, on_message
 from nonebot.adapters.cqhttp.bot import Bot
 from nonebot.adapters.cqhttp.event import Event, GroupMessageEvent,MessageEvent, PokeNotifyEvent
 from nonebot.adapters.cqhttp.message import Message, MessageSegment
 from nonebot.rule import to_me
 from nonebot.typing import T_State
+
 from wordcloud import WordCloud
-from PIL import Image
-from io import BytesIO
-import matplotlib.pyplot as plt
-import numpy as np
-import base64
+
+import jieba
+
 
 gpath = '/home/qqbot/plugindata/wordcloud'
 path = gpath +'/data.json'
 font = gpath + '/SourceHanSansHWSC-Regular.otf'
+punc = string.punctuation + "！？｡＂＃＄％＆＇（）＊＋，－／：；＜＝＞＠［＼］＾＿｀｛｜｝～｟｠｢｣､、〃》「」『』【】〔〕〖〗〘〙〚〛〜〝〞〟〰〾〿–—‘’‛“”„‟…‧﹏."
 
 
 data = {} #语料数据
@@ -76,9 +80,55 @@ async def generator_handle(bot: Bot, event: Event, state: T_State):
     print(text)
     img = WordCloud(background_color='white',mask=mask,font_path=font).generate(text)
     """
+    ###jieba中文分词
+    words = {}
+    for i in data[id]:
+        words_list = jieba.cut(i)
+        for j in words_list:
+            if j not in words and j not in punc:
+                words.setdefault(j,1)
+            elif j in words:
+                num = words[j]
+                words[j] = num + 1
+    print(words)
+    img = WordCloud(background_color='white',mask=mask,font_path=font).fit_words(words)
+    img = WordCloud.to_image(img)
+    buffer = BytesIO()
+    img.save(buffer, format='PNG')
+    pic = base64.b64encode(buffer.getvalue()).decode()
+    await bot.send(event,message=MessageSegment.image(f'base64://{pic}'))
 
-    word = data[id]
-    img = WordCloud(background_color='white',mask=mask,font_path=font).fit_words(word)
+generator = on_command('生成句云',aliases={"句云"})
+@generator.handle()
+async def generator_handle(bot: Bot, event: Event, state: T_State):
+    mask = np.array(Image.open(gpath + '/mask.png'))
+    group_id = event.group_id
+    id = union(group_id, 1)
+
+    """
+    #不用分词的话按照标点符号分割
+    text = ''
+    for i in data[id]:
+        t = data[id][i]
+        while t > 0:
+            text = text + i + ' '
+            t -= 1
+    print(text)
+    img = WordCloud(background_color='white',mask=mask,font_path=font).generate(text)
+    """
+
+    words = {}
+    for i in data[id]:
+        words_list = WordCloud()
+        words_list = WordCloud.process_text(words_list, i)
+        for j in words_list:
+            if j not in words:
+                words.setdefault(j,1)
+            elif j in words:
+                num = words[j]
+                words[j] = num + 1
+    print(words)
+    img = WordCloud(background_color='white',mask=mask,font_path=font).fit_words(words)
     img = WordCloud.to_image(img)
     buffer = BytesIO()
     img.save(buffer, format='PNG')
